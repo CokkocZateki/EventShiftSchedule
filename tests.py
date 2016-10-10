ddb2f853846d2dd4186215b61f71e596b74d343c
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2016-10-11 00:52:11 +0200

    Fix test2

--- tests.py
+++ tests.py
@@ -17,15 +17,17 @@ class NoEventTest(hyp_TestCase):
 
     def setUp(self):
         self.client = Client()
+        self.user = User.objects.create(username="John", password="abc")
+        self.client.force_login(self.user, backend=settings.AUTHENTICATION_BACKENDS[0])
 
     @given(checked=booleans(),
            time=integers(min_value=0, max_value=2**31-1),
            position=integers(min_value=0, max_value=2**31-1))
     def post_enter(self, status_code, checked, time, position):
-        self.user = User.objects.create(username="John", password="abc")
+
         #self.user = hyp_models(User, last_login=just("2099-12-31"), date_joined=just("2099-12-31")).example()
 
-        self.client.force_login(self.user, backend=settings.AUTHENTICATION_BACKENDS[0])
+
         response_enter = self.client.post(reverse('EventShiftSchedule:enter'), {
             "checked": checked,
             "time": time,
